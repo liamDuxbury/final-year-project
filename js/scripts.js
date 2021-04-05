d85c7f73c114d4ef864e44a2b6af4aa053e91ad9
Liam Duxbury <p16168273@my365.dmu.ac.uk>
2021-04-05 01:07:20 +0100

    Added tiles to home screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,43 @@
 "use strict";
 
+// import unirest from "unirest";
+
+// var req = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/479101/information");
+
+// function loadRecipes() {
+  
+//   req.headers({
+//     "x-rapidapi-key": "2c04ac924amshfdf390faa05e15ap152f4cjsncd6af456e587",
+//     "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+//     "useQueryString": true
+//   });
+  
+  
+//   req.end(function (res) {
+//     if (res.error) throw new Error(res.error);
+    
+//     console.log(res.body);
+//   });
+// }
+
 async function loadObject(id) {
   const url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`;
   const response = await fetch(url);
   return response.json();
 }
 
-function loadRecipes() {
-  const url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/extract?url=http%3A%2F%2Fwww.melskitchencafe.com%2Fthe-best-fudgy-brownies%2F"
-  const response = fetch(url, {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-key": "2c04ac924amshfdf390faa05e15ap152f4cjsncd6af456e587",
-      "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-    }
-  })
-  .then(response => {
-    console.log(response);
-  })
-  .catch(err => {
-    console.error(err);
-  });
-  return response
+async function loadSearch(myQuery) {
+  let baseURL = `https://collectionapi.metmuseum.org/public/collection/v1/search`;
+  const response = await fetch(`${baseURL}?hasImages=true&q=${myQuery}`);
+  return response.json();
 }
+
+async function doSearch() {
+  const result = await loadSearch(myQuery.value);
+  result.objectIDs.forEach(insertTile);
+}
+
+myQuery.addEventListener('change', doSearch);
 
 function buildTileFromData(obj) {
   const tile = document.createElement("article");
@@ -36,6 +51,6 @@ function buildTileFromData(obj) {
 async function insertTile(id) {
   const obj = await loadObject(id);
   const tile = buildTileFromData(obj);
-  results.appendChild(tile);
+  fpTiles.appendChild(tile);
 }
 
