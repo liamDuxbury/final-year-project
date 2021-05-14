4b294e5d8b02a7d16e71c3a1dc64a78f8090c8d1
Liam Duxbury <p16168273@my365.dmu.ac.uk>
2021-05-14 17:55:19 +0100

    Added DOM structure for recipe page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
-import { Recipe } from "../js/recipeClass";
+class Recipe{
+  constructor(){
+      this.cheap = null; // false
+      this.cookingMinutes = null; // 15
+      this.creditsText = null; // "Food.com"
+      this.cuisines = null; // (2) ["Japanese", "Asian"]
+      this.diets = null; // ["dairy free"]
+      this.dishTypes = null; // ["side dish"]
+      this.extendedIngredients = null; // Array(10)
+      this.glutenFree = null; // false
+      this.id = null; // 147625
+      this.image = null; // "https://spoonacular.com/recipeImages/147625-556x370.jpg"
+      this.occasions = null; // []
+      this.preparationMinutes = null; // 10
+      this.pricePerServing = null; // 114.97
+      this.readyInMinutes = null; // 25
+      this.servings = null; // 6
+      this.sourceUrl = null; // "http://www.food.com/recipe/tempura-bacon-237161"
+      this.summary = null; // "Tempura Bacon might be just the hor d'oeuvre you are searching for. This recipe serves 6. One serving contains <b>451 calories</b>, <b>13g of protein</b>, and <b>32g of fat</b>. For <b>$1.14 per serving</b>, this recipe <b>covers 10%</b> of your daily requirements of vitamins and minerals. Head to the store and pick up salt, pepper, garlic powder, and a few other things to make it today. 1 person has made this recipe and would make it again. It is a good option if you're following a <b>dairy free</b> diet. This recipe is typical of Japanese cuisine. From preparation to the plate, this recipe takes approximately <b>25 minutes</b>. All things considered, we decided this recipe <b>deserves a spoonacular score of 32%</b>. This score is not so awesome. Try <a href=\"https://spoonacular.com/recipes/bacon-tempura-salad-with-yogurt-and-honey-dressing-520971\">Bacon tempura salad with yogurt and honey dressing</a>, <a href=\"https://spoonacular.com/recipes/tempura-donburi-tendon-tempura-rice-bowl-88414\">Tempura Donburi - Tendon - Tempura Rice Bowl</a>, and <a href=\"https://spoonacular.com/recipes/tempura-350428\">Tempura</a> for similar recipes."
+      this.sustainable = null; // false
+      this.title = null; // "Tempura Bacon"
+      this.vegan = null; // false
+      this.vegetarian = null; // false
+  }
+} 
 
 let apiHeaders =  {
   "method": "GET",
@@ -11,7 +35,7 @@ let apiHeaders =  {
 const baseRecipeURL = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes`;
 
 const disoverTopics = ["Vegetarian", "Japanese", "Noodles", "Indian", "BBQ"]; 
-const tileIndex = 1;
+let tileIndex = 1;
 
 function generateHeader() {
   const template = document.createElement('template');
@@ -174,13 +198,33 @@ function generateDiscover() {
 function generateRecipePage() {
   const template = document.createElement('template');
   template.innerHTML = `
-  <p>This is the recipes page </p>
+  <section id="recipePage">
+    <section id="recipeTop">
+        <img id="recipeImage" src="../pictures/meatballs.jpg" alt="recipe-image">
+        <section id="quickSummary">
+            <h2 id="recipeTitle">Placeholder</h2>
+            <section id="recipeNumbers">
+                <h3 id="cookingTime">Cook Time</h3>
+                <h3 id="preperationTime">Prep Time</h3>
+                <h3 id="servings">Servings</h3>
+            </section>
+            <h3>Cuisines</h3>
+            <p id="recipeCuisines"></p>
+            <h3>Dietary Req.</h3>
+            <p id="dietaryRequirements"></p>
+        </section>
+    </section>
+    <section id="recipeSummary"></section>
+    <a id="URLToRecipe"></a>
+  </section>
   `
   document.body.appendChild(template.content);
-  let recipe = new Recipe();
-  console.log(recipe);
 } 
-  
+
+function populateRecipePage(recipeTile){
+  let recipe = localStorage.getItem(`${recipeName}*`);
+  recipeImage.src = recipe[image];
+}
 
 
 function generateDefault() {
@@ -241,12 +285,21 @@ async function buildTileFromData(recipe) {
   const tileContainer = document.createElement("section")
   const img = document.createElement("img");
   const recipeInformation = await getRecipeInfo(recipe.id);
-  debugger;
+  buildRecipePageData(recipeInformation);
   img.src = recipeInformation.image;
   img.alt = recipeInformation.title.substring(0, 20);
   tileContainer.appendChild(img);
   tile.appendChild(tileContainer);
   return tile;
+}
+
+function buildRecipePageData(recipeInformation){
+  let newRecipe = new Recipe();
+  for(let v in newRecipe) {
+    newRecipe[v] = recipeInformation[v];
+  }
+  const recipeObj = JSON.stringify(newRecipe);
+  localStorage.setItem(`${recipeInformation.title}`, recipeObj);
 }
 
 function buildTileData(tile) {
@@ -346,11 +399,6 @@ function clearResults() {
   const searchTiles = document.getElementsByClassName("searchTiles")[0];
   while(searchTiles.firstChild) {
     searchTiles.firstChild.remove();
-  }
-}
-
-function handle(e){
-  if(e.key === "Enter"){
   }
 }
 
